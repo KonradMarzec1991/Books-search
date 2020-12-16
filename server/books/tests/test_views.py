ab24ed5a70161ed3c4f7989aa70322e9a1758dc9
KonradMarzec1991 <konrimarzec@gmail.com>
2020-12-16 16:52:28 +0100

    Add reverse in tests

--- server/books/tests/test_views.py
+++ server/books/tests/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
 
@@ -7,14 +8,14 @@ from rest_framework.test import APIClient
 class TestBookList:
 
     def test_list(self):
-        response = APIClient().get('/books/')
+        response = APIClient().get(reverse('book-list'))
         queryset = response.data
         assert queryset['num_of_books'] == 2
         assert queryset['num_of_pages'] == 1
         assert len(queryset['books']) == 2
 
     def test_aggregation(self):
-        response = APIClient().get('/books/')
+        response = APIClient().get(reverse('book-list'))
         queryset = response.data['books']
         assert queryset[0]['rating'] == 3.33
         assert queryset[0]['review_count'] == 3
@@ -26,7 +27,7 @@ class TestBookList:
         assert queryset[0]['title'] == 'Ballada ptaków i węży'
 
     def test_retrieve(self):
-        response = APIClient().get('/books/2')
+        response = APIClient().get(reverse('book-retrieve', args=[2]))
         book = response.data
         assert book['title'] == 'Osiedle RZNiW'
         assert book['review_count'] == 0
